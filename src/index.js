207911367f29397d31bbd8e09e0113a1947beb65
fares-code <faresmohmed6872@gmail.com>
2026-01-30 22:47:53 +0200

    make updates for origin

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ app.use('/api/', limiter);
 
 // CORS middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001','https://sayad-alsamak-front-kima.vercel.app/'], // Specify allowed origins
+  origin: ['http://localhost:3000', 'http://localhost:3001','https://sayad-alsamak-front-kima.vercel.app'], // Specify allowed origins
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
